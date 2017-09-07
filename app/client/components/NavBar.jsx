37523a9eeb335de579943b7b556af7c6a3e65a37
Mikhail Metrikin <metrikin@gmail.com>
2017-09-06 23:02:04 -0700

    Working bug out of nav

--- app/client/components/NavBar.jsx
+++ app/client/components/NavBar.jsx
@@ -17,48 +17,7 @@ class NavBar extends Component {
   render() {
     return (
       <div class="mdl-layout mdl-js-layout">
-        <header class="mdl-layout__header mdl-layout__header--waterfall">
-          <div class="mdl-layout__header-row">
-            <span class="mdl-layout-title">Title</span>
-            <div class="mdl-layout-spacer" />
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-                  mdl-textfield--floating-label mdl-textfield--align-right">
-              <label
-                class="mdl-button mdl-js-button mdl-button--icon"
-                for="waterfall-exp"
-              >
-                <i class="material-icons">search</i>
-              </label>
-              <div class="mdl-textfield__expandable-holder">
-                <input
-                  class="mdl-textfield__input"
-                  type="text"
-                  name="sample"
-                  id="waterfall-exp"
-                />
-              </div>
-            </div>
-          </div>
-          <div class="mdl-layout__header-row">
-            <div class="mdl-layout-spacer" />
-            <nav class="mdl-navigation">
-              <a class="mdl-navigation__link" href="">
-                Link
-              </a>
-              <a class="mdl-navigation__link" href="">
-                Link
-              </a>
-              <a class="mdl-navigation__link" href="">
-                Link
-              </a>
-              <a class="mdl-navigation__link" href="">
-                Link
-              </a>
-            </nav>
-          </div>
-        </header>
-
-        {/* <header class="mdl-layout__header mdl-layout__header--waterfall mdl-layout__header portfolio-header">
+        <header class="mdl-layout__header mdl-layout__header--waterfall mdl-layout__header portfolio-header">
           <div class="mdl-layout__header-row portfolio-logo-row">
             <span class="mdl-layout__title">
               <span class="mdl-layout__title">Something should go here</span>
@@ -79,8 +38,8 @@ class NavBar extends Component {
               })}
             </nav>
           </div>
-        </header> */}
-        {/* <div class="mdl-layout__drawer mdl-layout--small-screen-only">
+        </header>
+        <div class="mdl-layout__drawer mdl-layout--small-screen-only">
           <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
             <a class="mdl-navigation__link is-active" href="/">
               Portfolio
@@ -95,7 +54,7 @@ class NavBar extends Component {
               Contact
             </a>
           </nav>
-        </div> */}
+        </div>
       </div>
     );
   }
