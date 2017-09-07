ca9c0b395f5f3010048937d2b035943ca90e3c96
Mikhail Metrikin <metrikin@gmail.com>
2017-09-06 19:59:23 -0700

    Minor Css changes

--- app/client/components/NavBar.jsx
+++ app/client/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import { h, Component } from 'preact';
+import * as style from './Header.style.css';
 
 const Header = ({ props }) => (
   <div className="mdl-layout mdl-js-layout">
-    <header className="mdl-layout__header mdl-layout__header--scroll">
+    <header
+      id="top-header"
+      className="mdl-layout__header mdl-layout__header--scroll"
+    >
       <div className="mdl-layout__header-row">
         <span className="mdl-layout-title">Title</span>
         <div className="mdl-layout-spacer" />
-        <nav className="mdl-navigation">
-          <a className="mdl-navigation__link" href="">
-            Link
-          </a>
+        <nav className="nav-links mdl-navigation">
           <a className="mdl-navigation__link" href="">
             Link
           </a>
@@ -25,9 +26,6 @@ const Header = ({ props }) => (
     <div className="mdl-layout__drawer">
       <span className="mdl-layout-title">Title</span>
       <nav className="mdl-navigation">
-        <a className="mdl-navigation__link" href="">
-          Link
-        </a>
         <a className="mdl-navigation__link" href="">
           Link
         </a>
